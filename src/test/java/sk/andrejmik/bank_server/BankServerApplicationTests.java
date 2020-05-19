cb3e75f5b6ea4e09100d1a83c5cdd75a949498e3
Andrej Mikoláš <andrej.mikolas@gmail.com>
2020-05-19 20:59:16 +0200

    Initial

---
+++ src/test/java/sk/andrejmik/bank_server/BankServerApplicationTests.java
@@ -0,0 +1,15 @@
+package sk.andrejmik.bank_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankServerApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
