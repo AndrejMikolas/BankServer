479b84a789ebe7d7c5cb74bf99d5a4b311d22b8c
Andrej Mikoláš <andrej.mikolas@gmail.com>
2020-05-20 19:57:45 +0200

    Add implementation of providers

--- src/main/java/sk/andrejmik/bank_server/BankServerApplication.java
+++ src/main/java/sk/andrejmik/bank_server/BankServerApplication.java
@@ -2,14 +2,18 @@ package sk.andrejmik.bank_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
+@SpringBootApplication(
+        scanBasePackages = {
+                "sk.andrejmik.bank_server"
+        }
+)
+@ComponentScan("sk.andrejmik.bank_server.*")
 public class BankServerApplication
 {
-
     public static void main(String[] args)
     {
         SpringApplication.run(BankServerApplication.class, args);
     }
-
 }
--- src/main/java/sk/andrejmik/bank_server/business_logic/implementation/CardProvider.java
+++ src/main/java/sk/andrejmik/bank_server/business_logic/implementation/CardProvider.java
@@ -1,18 +1,38 @@
 package sk.andrejmik.bank_server.business_logic.implementation;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import sk.andrejmik.bank_server.business_logic.interfaces.ICardProvider;
+import sk.andrejmik.bank_server.data_access.repository.ICardRepository;
 import sk.andrejmik.bank_server.entities.Card;
 
 import java.util.Collection;
 
+@Slf4j
 @Service
 public class CardProvider implements ICardProvider
 {
+    private final ICardRepository mCardRepository;
+
+    @Autowired
+    public CardProvider(ICardRepository cardRepository)
+    {
+        mCardRepository = cardRepository;
+    }
+
     @Override
     public boolean insert(Card data)
     {
-        return false;
+        try
+        {
+            mCardRepository.save(data);
+            log.info("Card saved");
+        } catch (Exception e)
+        {
+            return false;
+        }
+        return true;
     }
 
     @Override
@@ -24,18 +44,26 @@ public class CardProvider implements ICardProvider
     @Override
     public boolean update(Card data)
     {
-        return false;
+        try
+        {
+            mCardRepository.save(data);
+            log.info("Card updated");
+        } catch (Exception e)
+        {
+            return false;
+        }
+        return true;
     }
 
     @Override
     public void delete(Object id)
     {
-
+        mCardRepository.deleteById((Long) id);
     }
 
     @Override
     public Collection<Card> getAll()
     {
-        return null;
+        return mCardRepository.findAll();
     }
 }
